e56ff40f4768a2d9a2b15aee3a0f8cdc39e52071
Ameya Lokare <lokare.ameya@gmail.com>
2020-01-14 01:01:32 -0800

    WIP: encoding PUBLISH

--- src/main/java/com/github/juggernaut/macchar/packet/Publish.java
+++ src/main/java/com/github/juggernaut/macchar/packet/Publish.java
@@ -2,9 +2,13 @@ package com.github.juggernaut.macchar.packet;
 
 import com.github.juggernaut.macchar.ByteBufferUtil;
 import com.github.juggernaut.macchar.QoS;
+import com.github.juggernaut.macchar.property.MqttProperty;
 import com.github.juggernaut.macchar.property.PropertiesDecoder;
 
 import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author ameya
@@ -12,10 +16,10 @@ import java.nio.ByteBuffer;
 public class Publish extends MqttPacket {
 
     private final String topicName;
-    private final int packetId; // can be -1 if QoS = 0
+    private final Optional<Integer> packetId; // empty if QoS=0
     private final ByteBuffer payload;
 
-    protected Publish(int flags, String topicName, int packetId, final ByteBuffer payload) {
+    protected Publish(int flags, String topicName, Optional<Integer> packetId, final ByteBuffer payload) {
         super(PacketType.PUBLISH, flags);
         this.topicName = topicName;
         this.packetId = packetId;
@@ -27,7 +31,7 @@ public class Publish extends MqttPacket {
         final QoS qos = getQoS(flags);
         final String topicName = decodeTopicName(buffer);
         Utils.validateTopicName(topicName);
-        int packetId = -1;
+        Integer packetId = null;
         if (qos != QoS.AT_MOST_ONCE) {
             packetId = decodePacketIdentifier(buffer);
         }
@@ -35,7 +39,31 @@ public class Publish extends MqttPacket {
         final var payload = buffer.slice();
         // Make sure that we tell the decoder layer that we have read this buffer fully
         buffer.position(buffer.limit());
+        return new Publish(flags, topicName, Optional.ofNullable(packetId), payload);
+    }
+
+    public static Publish create(final QoS qos, final boolean dup, final boolean retain, final String topicName,
+                                 final Optional<Integer> packetId, final ByteBuffer payload) {
+        if (qos == QoS.AT_MOST_ONCE && packetId.isPresent()) {
+            throw new IllegalArgumentException("Packet ID cannot be specified if QoS is 0");
+        }
+        if (qos != QoS.AT_MOST_ONCE && packetId.isEmpty()) {
+            throw new IllegalArgumentException("Packet ID must be present if Qos > 0");
+        }
+        final int flags = encodeFlags(qos, dup, retain);
         return new Publish(flags, topicName, packetId, payload);
+    }
+
+    private static int encodeFlags(QoS qos, boolean dup, boolean retain) {
+        int flags = 0;
+        if (dup) {
+            flags |= 0x08;
+        }
+        flags |= (qos.getIntValue() << 1);
+        if (retain) {
+            flags |= 0x01;
+        }
+        return flags;
     }
 
     private static void validateFlags(int flags) {
@@ -61,24 +89,33 @@ public class Publish extends MqttPacket {
 
     @Override
     protected int getEncodedVariableHeaderLength() {
-        return 0;
+        // TODO: for now consider empty properties
+        final List<MqttProperty> properties = Collections.emptyList();
+        final int propertyLength = getEncodedPropertiesLength(properties);
+        return ByteBufferUtil.getEncodedUTF8StringLenghInBytes(topicName) +
+                packetId.map(p -> 2).orElse(0) +
+                ByteBufferUtil.getEncodedVariableByteIntegerLength(propertyLength) +
+                propertyLength;
     }
 
     @Override
     protected int getEncodedPayloadLength() {
-        return 0;
+        return payload.remaining();
     }
 
     @Override
     protected void encodeVariableHeader(ByteBuffer buffer) {
-
+        ByteBufferUtil.encodeUTF8String(buffer, topicName);
+        packetId.ifPresent(p -> ByteBufferUtil.encodeTwoByteInteger(buffer, p));
+        // TODO: just write a 0 property length for now
+        ByteBufferUtil.encodeVariableByteInteger(buffer, 0);
     }
 
     public String getTopicName() {
         return topicName;
     }
 
-    public int getPacketId() {
+    public Optional<Integer> getPacketId() {
         return packetId;
     }
 
